6589bea9e65eb8ead52528609c1a20c21a8727eb
Jeremy <jerry.gopp@gmx.de>
2020-08-17 03:21:05 +0200

    ulm.geojson etrs yes

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -3,15 +3,15 @@ const margin = { top: 50, left: 50, right: 50, bottom: 50 };
 //---MAP---\\
 const mapCanvas = document.getElementById('mapCanvas');
 var svg = d3.select(mapCanvas).append('svg')
-    .attr("width", 500)
-    .attr("height", 500);
+    .attr("width", 1000)
+    .attr("height", 1000);
 var mapWidth = +svg.attr("width"),
     mapHeight = +svg.attr("height");
 var projection = d3.geoMercator()
     .center([0, 0])
     .scale(100)
     .translate([mapWidth / 2, mapHeight / 2])
-d3.json("http://stadtplaene.ulm.de/geoserver/geoportalUlm/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoportalUlm:Stadtteile_generalisiert&srsName=EPSG:3875&outputFormat=json", function(data){
+d3.json("https://raw.githubusercontent.com/TheFloTheGame/Ulm-Baum/master/data/ulm.geojson?token=AMWD4TVCSHNZUV2XRTRM3JS7HHNT6", function(data){
     svg.append("g")
         .selectAll("path")
         .data(data.features)
